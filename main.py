963b8866fa75a11a44c97e05f23d19cf55e7a043
kingdreamerr <jackmvula458@gmail.com>
2023-01-13 20:17:25 +0200

    Fix error at line12

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def caser(direction, text,shift):
+def caser(direction,text,shift):
     end_text = ""
     if direction == "decode":
         shift *= -1
     for char in text:
         if char in alphabet:
             position = alphabet.index(char)
-            new_position = alphabet[position + shift]
+            new_position = position + shift
             end_text += alphabet[new_position]
         else:
             end_text += char
     print(f"your {direction}d message is {end_text}")
 
 should_continue = True
+print(logo)
 
 while should_continue:
     direction = input("Type 'encode' to encrypt type 'decode' to decrypt: \n")
     text = input("Type your message: \n").lower()
     shift = int(input("Type the shift number: \n"))
     shift = shift % 26
-    
-print(logo)
+
+    caser(direction,text,shift)
+    answer = input("Would you like to continue? yes or no: ").lower()
+    if answer == "no":
+        should_continue = False
+        print("Bye!!")

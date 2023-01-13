393970681e45485f22109880632938b93b3ff0fb
kingdreamerr <jackmvula458@gmail.com>
2023-01-13 19:54:20 +0200

    Add alphabet list

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caser(direction, text,shift):
+    
 print(logo)
-

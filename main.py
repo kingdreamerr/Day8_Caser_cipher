9aa65c884f4c10bfa7c396466d501094ceb9da46
kingdreamerr <jackmvula458@gmail.com>
2023-01-13 19:56:52 +0200

    Add code to check the direction of the cipher

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caser(direction, text,shift):
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
     
 print(logo)
